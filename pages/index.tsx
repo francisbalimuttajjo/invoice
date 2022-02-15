ce58f532e99c439cdae6f477fe6f61b6b5f08543
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-02-15 16:11:11 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      <Head>
+        <title>Invoice</title>
+        <meta name="invoicing" content="invoicing app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+   <div className="bg-blue-500 ml-8 dark rounded-md">
+       <h1 className='text-sm  ml-8'>helo</h1>
+   </div>
+   
+    </div>
+  )
+}
+
+export default Home
