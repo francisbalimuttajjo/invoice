eabb89a65a492dc3dc9303188d2213a81307293e
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-02-23 11:04:49 +0300

    added dark

--- components/Form.tsx
+++ components/Form.tsx
@@ -146,7 +146,7 @@ const n=new Date().toISOString().slice(0, 10)
                         onClick={props.hideForm}
                          className='bg-gray-200 text-sm  sm:text-sm font-bold text-gray-400 hover:text-gray-500 hover:bg-gray-300 rounded-3xl px-2 py-3  sm:px-4 sm:py-3 '>Discard</button>
                         <div className='flex right-0  absolute'>
-                            <button className={`${darkTheme ? 'hover:bg-opacity-40' : "hover:bg-opacity-90"} p-2 sm:px-4 py-3 text-sm  sm:text-sm text-gray-500 font-bold rounded-3xl bg-black bg-opacity-80 `}>Save as Draft</button>
+                            <button className= 'dark:hover:bg-opacity-30 hover:bg-opacity-90 p-2 sm:px-4 py-3 text-sm  sm:text-sm text-gray-500 font-bold rounded-3xl bg-black bg-opacity-80 '>Save as Draft</button>
                             <button className='bg-blue-700 ml-1 text-white opacity-70 text-sm  sm:text-sm font-bold px-4 py-3 rounded-3xl hover:bg-blue-600'>Save & Send</button>
 
                         </div>
