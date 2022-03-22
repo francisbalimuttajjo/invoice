3a45c2d73aae7493035cd32cfd4c3fce6644a0e3
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-03-22 12:31:38 +0300

    triming space from inputs

--- utils/Validation.js
+++ utils/Validation.js
@@ -1,14 +1,14 @@
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
-  issuerStreet: Yup.string().required("field is required *"),
-  receiverStreet: Yup.string().required("field is required *"),
-  issuerCity: Yup.string().required("field is required *"),
-  receiverCity: Yup.string().required("field is required *"),
-  description: Yup.string().required("field is required *"),
-  issuerCountry: Yup.string().required("field is required *"),
-  receiverCountry: Yup.string().required("field is required *"),
-  receiverName: Yup.string().required("field is required *"),
+  issuerStreet: Yup.string().trim().required("field is required *"),
+  receiverStreet: Yup.string().trim().required("field is required *"),
+  issuerCity: Yup.string().trim().required("field is required *"),
+  receiverCity: Yup.string().trim().required("field is required *"),
+  description: Yup.string().trim().required("field is required *"),
+  issuerCountry: Yup.string().trim().required("field is required *"),
+  receiverCountry: Yup.string().trim().required("field is required *"),
+  receiverName: Yup.string().trim().required("field is required *"),
   terms: Yup.number().required().min(1, "choose terms"),
   receiverEmail: Yup.string()
     .email("Invalid email format")
@@ -16,7 +16,7 @@ const validationSchema = Yup.object({
 
   items: Yup.array().of(
     Yup.object().shape({
-      name: Yup.string().required("Name is required *"),
+      name: Yup.string().trim().required("Name is required *"),
       qty: Yup.number().required().min(1, " required *"),
       price: Yup.number().required().min(1, " required *"),
     })
